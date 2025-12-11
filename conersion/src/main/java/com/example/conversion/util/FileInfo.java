65e6ddf1c2214b9013711fda3e00a0f182d0f910
Mahadev <mahadevWadar2001@gmail.com>
2025-12-11 20:34:57 +0530

    done

---
+++ conersion/src/main/java/com/example/conversion/util/FileInfo.java
@@ -0,0 +1,22 @@
+package com.example.conversion.util;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
+public class FileInfo {
+    private String fileName;
+    private LocalDateTime uploadTime;
+
+    public FileInfo(String fileName, LocalDateTime uploadTime) {
+        this.fileName = fileName;
+        this.uploadTime = uploadTime;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public LocalDateTime getUploadTime() {
+        return uploadTime;
+    }
+}
